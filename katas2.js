1bf6d59869e1a4e240659007f0c64208d8abb94a
Vilmar Gabriel <vilmargabriel96@gmail.com>
2021-09-13 09:26:56 -0300

    finalizando

--- katas2.js
+++ katas2.js
@@ -1,36 +1,34 @@
 // comece a criar a sua função add na linha abaixo
 
-function add(x, y){
-return (x + y)
+function add(a, b) {
+  return (a + b)
 }
-
-
 // descomente a linha seguinte para testar sua função
 //console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
 
 // comece a criar a sua função multiply na linha abaixo
-function multiply(a,b){
-    let x = 0
-    for(let i = 0; i<b; i++)
-       x += add(a, 0);
-    return x;
+function multiply(a, b) {
+  let x = 0
+  for (let i = 0; i < b; i++){
+      x = add(x, a)
   }
-  multiply(8,12)
+  return x
+}
   
-
+console.log(multiply(10, 5))
 // descomente a linha seguinte para testar sua função
  //console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
 
 
 // comece a criar a sua função power na linha abaixo
-function power(x,n){
-    let t = 1;
-    for(let i = 1; i<=n; i++)
-      t *= multiply(x,x)/x;
-    return t
-  }
-  power(3,4)
-  
+function power(a, b){
+  let x = 1
+  for (let i = 0; i < b; i++){
+      x = multiply(x, a)
+}
+return x
+}
+console.log(power(2, 3))
 
 // descomente a linha seguinte para testar sua função
 // console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
@@ -38,14 +36,14 @@ function power(x,n){
 
 // comece a criar a sua função factorial na linha abaixo
 
-function factorial(a){
-    let n = 1;
-    for(let i = 1; i<=a; i++)
-      n *= multiply(i, 1);
-    return n;  
-  }
-  factorial(6)
-  
+function fatorial(num){
+  let total = 1
+  for(let i = num; i > 1; i--){
+      total = multiply(total, [i])
+}
+return total
+}
+console.log (fatorial(5))
 // descomente a linha seguinte para testar sua função
 //console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
 
